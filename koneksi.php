8e4822f374844bdad4b728ac64a9c615a43b7a64
Irfanmkh <59212419+Irfanmkh@users.noreply.github.com>
2021-05-24 09:01:25 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+//database configurations
+define("DB_HOST", "localhost");
+define("DB_UNAME", "root");
+define("DB_PASS", "");
+define("DB_DNAME", "quizler");
+$conn = mysqli_connect(DB_HOST, DB_UNAME, DB_PASS, DB_DNAME);
